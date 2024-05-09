42c15702a7c3461f94c2f108c3f0b3958c500739
sazaam <sazaam@gmail.com>
2024-05-09 17:47:32 +0900

    testing on passing data to pureseried section

--- app.js
+++ app.js
@@ -76,11 +76,14 @@ const UTILS = require('./utils');
 /////////////////// SITE CONSTANTS
 const CONSTANTS = require('./constants');
 
-
+/////////////////// FIXTURES
+const fixtures = require('./fixtures');
 
 
 /////////////////// SERVER SETTINGS
 const server = require('./server');
+
+
 
 
 
@@ -461,264 +464,13 @@ let toJSON = (data) => {
 }
 //////////////// warning Opera request '/' x3 times !! while FF only once
 
-let footnav_fixtures = `{
-    "footlinks": [
-        {
-            "name": "Headquarters",
-            "children":[
-                {
-                    "name":"adress",
-                    "content":"1570 N Batavia St <br />Orange, CA<br />92867"
-                }
-            ]
-        },
-        {
-            "name": "Partners",
-            "children":[
-                {
-                    "name":"creativia",
-                    "content":"Creativia",
-                    "link":"#"
-                },
-                {
-                    "name":"ictc",
-                    "content":"ICT Convergence",
-                    "link":"#"
-                },
-                {
-                    "name":"researchcenter",
-                    "content":"Research Center",
-                    "link":"#"
-                },
-                {
-                    "name":"nslab",
-                    "content":"NS Lab",
-                    "link":"#"
-                },
-                {
-                    "name":"cognitgo",
-                    "content":"Cognitgo",
-                    "link":"#"
-                }
-            ]
-        },
-        {
-            "name": "Research",
-            "children":[
-                {
-                    "name":"patents-and-papers",
-                    "content":"Patents and Papers",
-                    "link":"/research/"
-                }
-            ]
-        },
-        {
-            "name": "Help",
-            "children":[
-                {
-                    "name":"faq",
-                    "content":"FAQ",
-                    "link":"/faq/"
-                },
-                {
-                    "name":"support",
-                    "content":"Support",
-                    "link":"/support/"
-                },
-                {
-                    "name":"tnc",
-                    "content":"Terms and Conditions",
-                    "link":"/legal/"
-                }
-            ]
-        }
-    ]
-}`
 
-let nav_fixtures = `{
-	"sections": [
-        {
-            "id": "1",
-            "name": "home",
-            "path": "home/",
-            "page": {
-                "id": "1",
-                "template": {
-                    "id": "1",
-                    "name": "top",
-                    "behavior": {
-                        "@focus": "home_focus",
-                        "@toggle": "home_toggle"
-                    }
-                }
-            },
-            "children":[
-                {
-                    "id": "101",
-                    "name": "purechain",
-                    "path": "home/purechain/",
-                    "page": {
-                        "template": {
-                            "behavior": {
-                                "@focus": "home_children_focus",
-                                "@toggle": "home_children_toggle"
-                            }
-                        }
-                    }
-                    
-                },
-                {
-                    "id": "102",
-                    "name": "purewallet",
-                    "path": "home/purewallet/",
-                    "page": {
-                        "template": {
-                            "behavior": {
-                                "@focus": "home_children_focus",
-                                "@toggle": "home_children_toggle"
-                            }
-                        }
-                    }
-                    
-                },
-                {
-                    "id": "103",
-                    "name": "pureseries",
-                    "path": "home/pureseries/",
-                    "page": {
-                        "template": {
-                            "behavior": {
-                                "@focus": "home_children_focus",
-                                "@toggle": "home_children_toggle"
-                            }
-                        }
-                    }
-                    
-                },
-                {
-                    "id": "104",
-                    "name": "pureworld",
-                    "path": "home/pureworld/",
-                    "page": {
-                        "template": {
-                            "behavior": {
-                                "@focus": "home_children_focus",
-                                "@toggle": "home_children_toggle"
-                            }
-                        }
-                    }
-                    
-                }
-            ]
-        },
-        {
-            "id": "2",
-            "name": "wallet",
-            "path": "wallet/",
-            "page": {
-                "id": "2",
-                "template": {
-                    "id": "1",
-                    "name": "top",
-                    "behavior": {
-                        "@focus": "top_section_focus",
-                        "@toggle": "top_section_toggle"
-                    }
-                }
-            }
-        },
-        {
-            "id": "3",
-            "name": "features",
-            "path": "features/",
-            "page": {
-                "id": "3",
-                "template": {
-                    "id": "1",
-                    "name": "top",
-                    "behavior": {
-                        "@focus": "top_section_focus",
-                        "@toggle": "top_section_toggle"
-                    }
-                }
-            }
-        }, 
-        {
-            "id": "4",
-            "name": "learn",
-            "path": "learn/",
-            "page": {
-                "id": "4",
-                "template": {
-                    "id": "1",
-                    "name": "top",
-                    "behavior": {
-                        "@focus": "top_section_focus",
-                        "@toggle": "top_section_toggle"
-                    }
-                }
-            }
-        },
-        {
-            "id": "5",
-            "name": "faq",
-            "path": "faq/",
-            "data": {
-                "hidefrommenu":true
-            },
-            "page": {
-                "id": "5",
-                "template": {
-                    "id": "2",
-                    "name": "top",
-                    "behavior": {
-                        "@focus": "top_section_focus",
-                        "@toggle": "top_section_toggle"
-                    }
-                }
-            }
-        },
-        {
-            "id": "6",
-            "name": "legal",
-            "path": "legal/",
-            "data": {
-                "hidefrommenu":true
-            },
-            "page": {
-                "id": "6",
-                "template": {
-                    "id": "2",
-                    "name": "top",
-                    "behavior": {
-                        "@focus": "top_section_focus",
-                        "@toggle": "top_section_toggle"
-                    }
-                }
-            }
-        },
-        {
-            "id": "7",
-            "name": "research",
-            "path": "research/",
-            "data": {
-                "hidefrommenu":true
-            },
-            "page": {
-                "id": "7",
-                "template": {
-                    "id": "2",
-                    "name": "top",
-                    "behavior": {
-                        "@focus": "top_section_focus",
-                        "@toggle": "top_section_toggle"
-                    }
-                }
-            }
-        }
-    ]
-}` ;
 
+let footnav_fixtures = fixtures.footnav ;
+
+let nav_fixtures = fixtures.nav ;
+
+let pureseries_fixtures = fixtures.pureseries ;
 
 let root = async(req, res) => {
     
@@ -729,7 +481,10 @@ let root = async(req, res) => {
     
     let ttt = JSON.parse(footnav_fixtures) ;
     footnavlinks = cleanup(ttt, 'footlinks') ;
-    // console.log(topsections) ;
+
+    let tttt = JSON.parse(pureseries_fixtures) ;
+    pureseries_content = cleanup(tttt) ;
+    
     
     await res.render(path.join(__dirname, 'public/jade/index'), jadeparams.merge(jadeparams, {
         
@@ -739,6 +494,7 @@ let root = async(req, res) => {
         lang: 'en',
         topsections: toJSON(topsections),
         footnavlinks: toJSON(footnavlinks),
+        pureseries_content:toJSON(pureseries_content)
     })) ;
     
 }
