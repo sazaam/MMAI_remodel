fb2d1b1b6962209ad6f82a6bb56a4108874ecfca
yunburger <ybpark@nslab.tech>
2024-06-17 10:03:54 +0900

    chart error fix

--- public/js/chart.js
+++ public/js/chart.js
@@ -1,22 +1,23 @@
 const ctx = document.getElementById('myChart');
 
-  new Chart(ctx, {
-    type: 'line',
-    data: {
-      datasets: [{
-        label: "price",
-        data: [{x:"2024-06-13", y:20}, {x:"2024-06-14", y:20}, {x:"2024-06-15", y:20}, {x:"2024-06-16", y:20}, {x:"2024-06-17", y:20}, {x:"2024-06-18", y:20}, {x:"2024-06-18-20:22", y:550}] //data: [{x: '2016-12-25', y: 20}, {x: '2016-12-26', y: 10}]
-    }]
-    },
-    options: {
-        scales:{
-            y:{
-                title:{
-                    display:true,
-                    text:"USD",
 
-                }
-            }
+new Chart(ctx, {
+  type: 'line',
+  data: {
+    datasets: [{
+      label: "price",
+      data: [{ x: "2024-06-13", y: 20 }, { x: "2024-06-14", y: 20 }, { x: "2024-06-15", y: 20 }, { x: "2024-06-16", y: 20 }, { x: "2024-06-17", y: 20 }, { x: "2024-06-18", y: 20 }, { x: "2024-06-18-20:22", y: 550 }] //data: [{x: '2016-12-25', y: 20}, {x: '2016-12-26', y: 10}]
+    }]
+  },
+  options: {
+    scales: {
+      y: {
+        title: {
+          display: true,
+          text: "USD",
+
         }
+      }
     }
-  });
+  }
+});
